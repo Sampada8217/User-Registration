edf22f6257ba00f772409157d0f081cfe6af9155
sampada8217 <sampadatirthkar@gmail.com>
2020-04-24 21:21:49 +0530

    Added Code to Check Mobile Format is Valid or Not

--- user.sh
+++ user.sh
@@ -5,11 +5,12 @@ echo "Enter Last Name"
 read last
 echo "Enter Email_id"
 read email
-
+echo "Enter Mobile no"
+read mobiNum
 patName="^[A-Z][a-z]{3,}$"
 patLastName="^[A-Z][a-z]{3,}$"
 patEmailId="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
-
+patMobileNumber="^[1-9]{2}[[:space:]]{1}[0-9]{10}$"
 function checkValidUserData()
 {
 	if [[ $1 =~ $2 ]]
@@ -19,6 +20,17 @@ function checkValidUserData()
 		echo "Invalid User Data"
 	fi
 }
+function checkValidMobiNum()
+{
+	if [[ $mobiNum =~ $patMobileNumber ]]
+	then
+		echo "Valid Mobile Number"
+	else
+		echo "Invalid Mobile Number"
+	fi
+}
 checkValidUserData $name $patName
 checkValidUserData $last $patLastName
 checkValidUserData $email $patEmailId
+checkValidMobiNum $mobiNum $patMobileNumber
+
