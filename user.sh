a4a556c8bf2b4fd45fb1dfd74a4993defaf8f541
sampada8217 <sampadatirthkar@gmail.com>
2020-04-24 21:34:31 +0530

    Added Code to Check Valid Password Rule1

--- user.sh
+++ user.sh
@@ -7,10 +7,13 @@ echo "Enter Email_id"
 read email
 echo "Enter Mobile no"
 read mobiNum
+echo "Enter Password"
+read pass
 patName="^[A-Z][a-z]{3,}$"
 patLastName="^[A-Z][a-z]{3,}$"
 patEmailId="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
 patMobileNumber="^[1-9]{2}[[:space:]]{1}[0-9]{10}$"
+patPassRule1="^[A-Za-z]{8,}$"
 function checkValidUserData()
 {
 	if [[ $1 =~ $2 ]]
@@ -33,4 +36,4 @@ checkValidUserData $name $patName
 checkValidUserData $last $patLastName
 checkValidUserData $email $patEmailId
 checkValidMobiNum $mobiNum $patMobileNumber
-
+checkValidUserData $pass $patPassRule1
