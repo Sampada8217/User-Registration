5181e5a401e2d6b408d3a8f21267b3d9ddb25077
sampada8217 <sampadatirthkar@gmail.com>
2020-04-24 20:39:18 +0530

    Added Code to Check Email ID is Valid or Not

--- user.sh
+++ user.sh
@@ -3,9 +3,12 @@ echo "Enter First Name"
 read name
 echo "Enter Last Name"
 read last
+echo "Enter Email_id"
+read email
 
 patName="^[A-Z][a-z]{3,}$"
 patLastName="^[A-Z][a-z]{3,}$"
+patEmailId="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
 
 function checkValidUserData()
 {
@@ -18,3 +21,4 @@ function checkValidUserData()
 }
 checkValidUserData $name $patName
 checkValidUserData $last $patLastName
+checkValidUserData $email $patEmailId
